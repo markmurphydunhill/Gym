68187d498d1c80354d2acacb86579b90cd3e3a6a
Glitch (gym3) <none>
2018-08-29 13:29:24 +0000

    Gym V3

--- controllers/assessment.js
+++ controllers/assessment.js
@@ -2,16 +2,44 @@
 
 const logger = require('../utils/logger');
 const assessmentStore = require('../models/assessment-store.js');
+const uuid = require('uuid');
 
 const assessment = {
   index(request, response) {
     const assessmentId = request.params.id;
     logger.debug('Assessment id = ', assessmentId);
     const viewData = {
-      title: 'Assessment List',
+      title: 'Assessment List???',
       assessmentDetails: assessmentStore.getAssessment(assessmentId),
     };
     response.render('assessmentlist', viewData);
+  },
+  
+  
+ deleteAssessment(request, response) {
+    const assessmentlistId = request.params.id;
+    const assessmentId = request.params.assessmentid;
+    logger.debug(`Deleting Assessment ${assessmentId} from AssessmentList ${assessmentlistId}`);
+    assessmentStore.removeAssessment(assessmentlistId, assessmentId);
+    response.redirect('/assessment/' + assessmentlistId);
+  
+  },
+  
+ addAssessment(request, response) {
+    const assessmentlistId = request.params.id;
+    const assessment = assessmentStore.getAssessment(assessmentlistId);
+    const newAssessment = {
+      id: uuid(),
+      weight: request.body.weight,
+      chest: request.body.chest,
+      thigh: request.body.thigh,
+      upperarm: request.body.upperarm,
+      waist: request.body.waist,
+      hips: request.body.hips,
+    };
+    logger.debug('New Assessment = ', newAssessment);
+    assessmentStore.addAssessment(assessmentlistId, newAssessment);
+    response.redirect('/assessment/' + assessmentlistId);
   },
 
  
--- routes.js
+++ routes.js
@@ -11,5 +11,8 @@ router.get('/', dashboard.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
 router.get('/assessment/:id', assessment.index);
+router.get('/assessment/:id/deleteAssessment/:assessmentid', assessment.deleteAssessment);
+router.post('/assessment/:id/addassessment', assessment.addAssessment);
+
 
 module.exports = router;
