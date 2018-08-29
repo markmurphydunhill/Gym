ac8c39bb0674ab67448e402e2a77b0138bd2fda4
Glitch (gym5) <none>
2018-08-29 13:41:34 +0000

    Gym v5

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,20 +1,41 @@
 'use strict';
 
+const accounts = require ('./accounts.js');
 const logger = require('../utils/logger');
-const assessmentStore = require('../models/assessment-store.js');
+const assessmentStore = require('../models/assessment-store');
+const uuid = require('uuid');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
+    const loggedInUser = accounts.getCurrentUser(request);
     const viewData = {
-      title: 'Dashboard',
-      assessmentCompleted: assessmentStore.getAllAssessments(),
+      title: 'Playlist Dashboard',
+      assessmentlist: assessmentStore.getUserAssessmentlists(loggedInUser.id),
     };
-    logger.info('about to render', assessmentStore.getAllAssessments());
+    logger.info('about to render', assessmentStore.getUserAssessmentlists(loggedInUser.id));
     response.render('dashboard', viewData);
   },
+
+  /*deletePlaylist(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Deleting Playlist ${playlistId}`);
+    playlistStore.removePlaylist(playlistId);
+    response.redirect('/dashboard');
+  },
+
+  addPlaylist(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
+    const newPlayList = {
+      id: uuid(),
+      userid: loggedInUser.id,
+      title: request.body.title,
+      songs: [],
+    };
+    logger.debug('Creating a new Playlist', newPlayList);
+    playlistStore.addPlaylist(newPlayList);
+    response.redirect('/dashboard');
+  },*/
 };
-
-
 
 module.exports = dashboard;
--- routes.js
+++ routes.js
@@ -3,16 +3,25 @@
 const express = require('express');
 const router = express.Router();
 
+const accounts = require('./controllers/accounts.js');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
-const assessment = require('./controllers/assessment.js');
+const playlist = require('./controllers/playlist.js');
 
-router.get('/', dashboard.index);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
 router.get('/dashboard', dashboard.index);
-router.get('/about', about.index);
-router.get('/assessment/:id', assessment.index);
-router.get('/assessment/:id/deleteAssessment/:assessmentid', assessment.deleteAssessment);
-router.post('/assessment/:id/addassessment', assessment.addAssessment);
+//router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
+//router.post('/dashboard/addplaylist', dashboard.addPlaylist);
 
+router.get('/about', about.index);
+//router.get('/playlist/:id', playlist.index);
+//router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
+//router.post('/playlist/:id/addsong', playlist.addSong);
 
 module.exports = router;
